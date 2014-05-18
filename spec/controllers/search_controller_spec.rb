67a876217de3beb9c4f055144856bbf07dbe9da9
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 13:37:37 +0200

    zaczynamy testy

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe SearchController do
 
-  before(:each) do
-    @movie = Tmdb::Movie.find('batman')
-  end
-    
-  it "should return movies" do
-    response.should be_success
-  end
+ 
 
 end
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,29 +1,5 @@
 require 'spec_helper'
 describe "home/index.html.erb" do
  
-  before(:each) do
-    @configuration = Tmdb::Configuration.new
-    @now_playing = Tmdb::Movie.now_playing
-  end
-
-  it "displays search" do
-    render
-    expect(rendered).to include("Now Playing")
-  end
-
-  it "displays search text?" do
-    render
-    expect(rendered).to include("Search for a movie")
-  end
-
-  it "displays search input correctly?" do
-    render
-    expect(rendered).to include("input-xlarge searchInput")
-  end
-
-  it "displays images correctly?" do
-    render
-    expect(rendered).to include("http://image.tmdb.org/")
-  end
 
 end
