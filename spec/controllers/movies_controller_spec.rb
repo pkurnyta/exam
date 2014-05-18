58cec3f8c6b4e80e4a4f69f092fc916366405d0a
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 14:15:51 +0200

    test

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe MoviesController do
 
+before do
+    @movie = Tmdb::Movie.detail(268321)
+  end
+
+  it "should return an id" do
+    @movie.id.should eq(268321)
+  end
   
     
 end
