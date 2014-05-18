1dd199ad97f152f537760a4072ed4ea782634148
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 19:57:56 +0200

    testy 84-85

--- spec/views/people/show.html.erb_spec.rb
+++ spec/views/people/show.html.erb_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 describe "people/show.html.erb" do
+
+ before(:each) do
+    @configuration = Tmdb::Configuration.new
+ 	@people = Tmdb::Person.detail(1014932)
+  	@movies = Tmdb::Person.credits(1014932)
+  end
+
+  it "displays name correctly?" do
+    render
+    expect(rendered).to include("Gabriella Wilde")
+  end
+
+  it "displays image correctly?" do
+    render
+    expect(rendered).to include("http://image.tmdb.org/t/p/w154/xbhScoMy3P6pK8Eg5V3EbGoxRSg.jpg")
+  end
  
 end
