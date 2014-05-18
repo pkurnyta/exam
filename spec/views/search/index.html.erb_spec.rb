390d50e72d299bef49b63b32141ad67a8536ea17
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 20:39:29 +0200

    testy 95-100

--- spec/views/search/index.html.erb_spec.rb
+++ spec/views/search/index.html.erb_spec.rb
@@ -1,6 +1,40 @@
 require 'spec_helper'
 describe "search/index.html.erb" do
  
-  
+  before(:each) do
+    @configuration = Tmdb::Configuration.new
+    @movie = Tmdb::Movie.find('Apocalypto')
+  end
+
+  it "displays title?" do
+    render
+    expect(rendered).to include("Search results")
+  end
+
+  it "displays batman indeed?" do
+    render
+    expect(rendered).to include("Apocalypto")
+  end
+
+  it "displays year of production correctly?" do
+    render
+    expect(rendered).to include("(2006)")
+  end
+
+  it "displays image correctly?" do
+    render
+    expect(rendered).to include("http://image.tmdb.org/t/p/w154/5BTFXR96hcBzmJvd9FwNayV79Xu.jpg")
+  end
+
+  it "displays writers correctly?" do
+    render
+    expect(rendered).to include("Rudy Youngblood")
+  end
+
+  it "displays search directors correctly?" do
+    render
+    expect(rendered).to include("Mel Gibson")
+  end
+
 
 end
