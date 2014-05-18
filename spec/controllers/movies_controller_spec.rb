5c8208f38432e1ad1d36192e841a426c56d9cd58
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 14:36:48 +0200

    testy 3-5

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -9,6 +9,17 @@ before do
   it "should return an id" do
     @movie.id.should eq(268321)
   end
-  
+
+  it "should id be int" do
+    @movie.id.is_a? Integer
+  end
+
+  it "should return a imdb id" do
+    @movie.imdb_id.should eq("tt1300854")
+  end
+
+  it "should imdb id be string" do
+    @movie.imdb_id.is_a? String
+  end  
     
 end
