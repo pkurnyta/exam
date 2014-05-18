62a7c4d1afea53745cadf2633361cd3f593a4d54
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 14:50:31 +0200

    testy 6-10

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -22,4 +22,24 @@ before do
     @movie.imdb_id.is_a? String
   end  
     
+it "should return an original title" do
+    @movie.original_title.should eq("Squatters")
+  end
+
+  it "should original title be string" do
+    @movie.original_title.is_a? String
+  end
+
+  it "should return if for adults" do
+    @movie.adult.should eq(false)
+  end
+
+  it "should return a backdrop path" do
+    @movie.backdrop_path.should eq("/n9X2DKItL3V0yq1q1jrk8z5UAki.jpg")
+  end
+
+  it "should backdrop path be string" do
+    @movie.backdrop_path.is_a? String
+  end
+
 end
