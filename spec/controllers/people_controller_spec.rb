c3ba74088f0d43bab753d453d29246d9c4b17349
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 17:10:19 +0200

    testy 34-38

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe PeopleController do
 
-  
+before do
+    @person = Tmdb::Person.detail(137424)
+  end
+
+  it "should return an id" do
+    @person.id.should eq(137424)
+  end
+
+  it "should an id be int" do
+    @person.id.is_a? Integer
+  end
+
+  it "should return a false" do
+    @person.adult.should eq(false)
+  end
+
+  it "should be a array" do
+    @person.also_known_as.kind_of?(Array)
+  end
+
+  it "should return a name" do
+    @person.name.should eq("Gia Mantegna")
+  end  
 
 end
