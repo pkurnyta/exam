c81c0a24e569b04489b4ed9ab150fde8aaceaa3f
pkurnyta <p.kurnyta@gmail.com>
2014-05-18 18:05:10 +0200

    test search (51)

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe SearchController do
 
- 
+before(:each) do
+    @movie = Tmdb::Movie.find('Captain America')
+  end
+    
+  it "should return movies" do
+    response.should be_success
+  end 
 
 end
